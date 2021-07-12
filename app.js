0dd82bdf4e4405b7a0e1891e38556cb239c1b54d
guciwebsite <prasetyoagung1001@gmail.com>
2021-07-12 15:50:45 +0700

    project

---
+++ app.js
@@ -0,0 +1,175 @@
+const { Client } = require('whatsapp-web.js');
+const express = require('express');
+const { body, validationResult } = require('express-validator');
+const socketIo = require('socket.io');
+const http = require('http');
+const qrcode = require('qrcode');
+const { phoneNumberFormatter } = require('./helpers/formatter');
+const fs = require('fs');
+// const client = new Client();
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.get('/', (req, res,) => {
+	// res.status(200).json({
+	// 	status: true,
+	// 	message: 'Hello World'
+	// })
+	res.sendFile('index.html', {
+    root: __dirname
+  });
+})
+
+
+const SESSION_FILE_PATH = './whatsapp-session.json';
+let sessionCfg;
+if (fs.existsSync(SESSION_FILE_PATH)) {
+    sessionCfg = require(SESSION_FILE_PATH);
+}
+
+const client = new Client({
+  restartOnAuthFail: true,
+  puppeteer: {
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process', // <- this one doesn't works in Windows
+      '--disable-gpu'
+    ],
+  },
+  session: sessionCfg
+});
+
+
+client.on('message', async msg => {
+    console.log('MESSAGE RECEIVED', msg);
+    if (msg.body === 'p') {
+        // Send a new message as a reply to the current one
+        msg.reply('pong');
+
+    } else if (msg.body === '!ping') {
+        // Send a new message to the same chat
+        client.sendMessage(msg.from, 'pong');
+    }
+});
+
+client.initialize();
+
+io.on('connection', function(socket) {
+socket.emit('message', 'Connecting...');
+
+client.on('qr', (qr) => {
+    console.log('QR RECEIVED', qr);
+    qrcode.toDataURL(qr, (err, url) => {
+      socket.emit('qr', url);
+      socket.emit('message', 'QR Code received, scan please!');
+    });
+  });
+
+client.on('ready', () => {
+    socket.emit('ready', 'Whatsapp is ready!');
+    socket.emit('message', 'Whatsapp is ready!');
+  });
+client.on('authenticated', (session) => {
+    socket.emit('authenticated', 'Whatsapp is authenticated!');
+    socket.emit('message', 'Whatsapp is authenticated!');
+    console.log('AUTHENTICATED', session);
+    sessionCfg = session;
+    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function(err) {
+      if (err) {
+        console.error(err);
+      }
+    });
+  });
+
+  client.on('auth_failure', function(session) {
+    socket.emit('message', 'Auth failure, restarting...');
+  });
+
+  client.on('disconnected', (reason) => {
+    socket.emit('message', 'Whatsapp is disconnected!');
+    fs.unlinkSync(SESSION_FILE_PATH, function(err) {
+        if(err) return console.log(err);
+        console.log('Session file deleted!');
+    });
+    client.destroy();
+    client.initialize();
+  });
+
+});
+
+const checkRegisteredNumber = async function(number) {
+  const isRegistered = await client.isRegisteredUser(number);
+  return isRegistered;
+}
+
+// Send message
+// app.post('/send-message', [
+//   body('number').notEmpty(),
+//   body('message').notEmpty(),
+// ], async (req, res) => {
+//   const errors = validationResult(req).formatWith(({
+//     msg
+//   }) => {
+//     return msg;
+//   });
+
+//   if (!errors.isEmpty()) {
+//     return res.status(422).json({
+//       status: false,
+//       message: errors.mapped()
+//     });
+//   }
+
+//   const number = phoneNumberFormatter(req.body.number);
+//   const message = req.body.message;
+
+//   const isRegisteredNumber = await checkRegisteredNumber(number);
+
+//   if (!isRegisteredNumber) {
+//     return res.status(422).json({
+//       status: false,
+//       message: 'The number is not registered'
+//     });
+//   }
+// // app.post('/send-message', (req, res) => {
+// // 	const number = req.body.number;
+// // 	const message = req.body.message;
+
+//   client.sendMessage(number, message).then(response => {
+//     res.status(200).json({
+//       status: true,
+//       response: response
+//     });
+//   }).catch(err => {
+//     res.status(500).json({
+//       status: false,
+//       response: err
+//     });
+//   });
+// });
+
+// client.on('message_create', (msg) => {
+//     // Fired on all message creations, including your own
+//     if (msg.fromMe) {
+//         // do stuff here
+//         console.log(msg)
+//     }
+// });
+
+
+server.listen(8000, function(){
+	console.log('app run' + 8000)
+})
